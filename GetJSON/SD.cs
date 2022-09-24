0b7f61b69ad3bbc377fccb874c22d9da8dcd9467
ducthaopham <onephamtx@gmail.com>
2022-09-25 04:26:03 +0700

    Delete GetJson

--- GetJSON/SD.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GetJSON
-{
-    public class SD
-    {
-        public static string APIBaseUrl = "https://localhost:44346/";
-        public static string NationalParkAPIPath = APIBaseUrl + "api/v1/nationalparks";
-    }
-}
